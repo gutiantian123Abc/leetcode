9005c8262b301c87804f5d06d0e48a013fb6c1a5
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-06-15 16:47:51 -0400

    Connecting Graph III

--- dataStructures/UnionFind/Connecting_Graph.java
+++ dataStructures/UnionFind/Connecting_Graph.java
@@ -120,3 +120,83 @@ public class ConnectingGraph2 {
         return size[root_a];
     }
 }
+
+
+
+
+
+/*Connecting Graph III 
+Given n nodes in a graph labeled from 1 to n. There is no edges in the graph at beginning.
+
+You need to support the following method:
+1. connect(a, b), an edge to connect node a and node b
+2. query(), Returns the number of connected component in the graph
+
+Example
+5 // n = 5
+query() return 5
+connect(1, 2)
+query() return 4
+connect(2, 4)
+query() return 3
+connect(1, 4)
+query() return 3
+*/
+
+public class ConnectingGraph3 {
+    private int[] father;
+    private int count;
+
+    private int find(int x) {
+        if(father[x] == x) {
+            return x;
+        }
+        
+        father[x] = find(father[x]);
+        
+        return father[x];
+    }
+    
+    
+    public ConnectingGraph3(int n) {
+        // initialize your data structure here.
+        father = new int[n + 1];
+        count = n;
+        
+        for(int i = 1; i < n + 1; i++) {
+            father[i] = i;
+        }
+    }
+
+    public void connect(int a, int b) {
+        // Write your code here
+        int root_a = find(a);
+        int root_b = find(b);
+        
+        if(root_a != root_b) {
+            father[root_b] = root_a;
+            count--;
+        }
+    }
+        
+    public int query() {
+        // Write your code here
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
