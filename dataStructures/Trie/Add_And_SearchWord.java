375dd852a8d3d4822592aed6a49d88aceaa15557
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-06-25 20:58:04 -0400

    Add DFS + Trie Search Word

--- dataStructures/Trie/Add_And_SearchWord.java
+++ dataStructures/Trie/Add_And_SearchWord.java
@@ -124,3 +124,113 @@ public class WordDictionary {
 // wordDictionary.addWord("word");
 // wordDictionary.search("pattern");
 
+
+
+/* DFS version
+*/
+public class WordDictionary {
+    public class TrieNode {
+        public char c;
+        public HashMap<Character, TrieNode> children;
+        public boolean hasWord;
+        
+        public TrieNode() {
+            children = new HashMap<Character, TrieNode>();
+            hasWord = false;
+        }
+        
+        public TrieNode(char c) {
+            this.c = c;
+            children = new HashMap<Character, TrieNode>();
+            hasWord = false;
+            
+        }
+    }
+    
+    private TrieNode root;
+    
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        // Write your code here
+        TrieNode curNode = root;
+        char[] wordArray = word.toCharArray();
+        HashMap<Character, TrieNode> children = root.children;
+        for(int i = 0; i < wordArray.length; i++) {
+            char curWord = wordArray[i];
+            if(!children.containsKey(curWord)) {
+                TrieNode newNode = new TrieNode(curWord);
+                children.put(curWord, newNode);
+                curNode = newNode;
+            }else {
+                curNode = children.get(curWord);
+            }
+            
+            children = curNode.children;
+            
+            if(i == wordArray.length - 1) {
+                curNode.hasWord = true;
+            }
+        }
+    }
+
+
+
+    private boolean find(char[] wordArray, int index, TrieNode curNode) {
+        
+        char letter = wordArray[index];
+        if(index == wordArray.length - 1) {
+            if(letter == '.') {
+                if(curNode.children.size() > 0) {
+                    boolean flag = false;
+                    for(TrieNode child : curNode.children.values()) {
+                        if(child.hasWord) {
+                            flag = true;
+                        }
+                    }
+                    return flag;
+                }else {
+                    return false;
+                }
+            }else {
+                if(curNode.children.containsKey(letter)) {
+                    return curNode.children.get(letter).hasWord;
+                }else {
+                    return false;
+                }
+            }
+        }
+        
+        if(letter == '.') {
+            if(curNode.children.size() == 0) {
+                return false;
+            }else {
+                boolean flag = false;
+                for(TrieNode child : curNode.children.values()) {
+                    if(find(wordArray, index + 1, child)) {
+                        flag = true;
+                    }
+                }
+                return flag;
+            }
+        }else {
+            if(curNode.children.containsKey(letter)) {
+                return find(wordArray, index + 1, curNode.children.get(letter));
+            }else {
+                return false;
+            }
+        }
+    }
+    
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        // Write your code here
+        return find(word.toCharArray(), 0, root);
+    }
+}
+
