e9b93e1a32f5ca7f50441050ec0230127461077d
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-07-27 16:07:58 -0400

    Add Unique_Paths II

--- Dynamic_Programming/坐标型动态规划/Unique_Paths.java
+++ Dynamic_Programming/坐标型动态规划/Unique_Paths.java
@@ -1,4 +1,4 @@
-s/* Unique Paths
+/* Unique Paths
 A robot is located at the top-left corner of a m x n grid.
 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid.
 How many possible unique paths are there?
@@ -37,3 +37,75 @@ public class Solution {
         
     }
 }
+
+
+
+/* Unique Paths II
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+m and n will be at most 100.
+
+Example
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+*/
+
+public class Solution {
+    /**
+     * @param obstacleGrid: A list of lists of integers
+     * @return: An integer
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // write your code here
+        if(obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        int[][] arr = new int[m][n];
+  
+        
+        for(int i = 0; i < m; i++) {
+            if(obstacleGrid[i][0] == 1) {
+                break;
+            }else {
+                arr[i][0] = 1;
+            }
+        }
+        
+        for(int j = 0; j < n; j++) {
+            if(obstacleGrid[0][j] == 1) {
+                break;
+            }else {
+                arr[0][j] = 1;
+            }
+        }
+        
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    continue;
+                }else {
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
+            }
+        }
+        
+        return arr[m - 1][n - 1];
+    }
+}
+
+
