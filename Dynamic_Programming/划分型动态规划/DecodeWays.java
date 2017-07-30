cf083bac049eceba88d74de8e916ab7ad3458f89
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-07-30 14:19:53 -0400

    reconstructured

--- Dynamic_Programming/划分型动态规划/DecodeWays.java
+++ Dynamic_Programming/划分型动态规划/DecodeWays.java
@@ -1,13 +1,11 @@
-/* Decode Ways
+/* Decode Ways 
 A message containing letters from A-Z is being encoded to numbers using the following mapping:
 
 'A' -> 1
 'B' -> 2
 ...
 'Z' -> 26
-
 Given an encoded message containing digits, determine the total number of ways to decode it.
-
 Example
 Given encoded message 12, it could be decoded as AB (1 2) or L (12).
 The number of ways decoding 12 is 2.
@@ -31,19 +29,17 @@ public class Solution {
         char[] word = s.toCharArray();
         
         for(int i = 1; i < f.length; i++) {
-        	//妙！
             if(word[i - 1] != '0') {
                 f[i] += f[i - 1];
             }
             
-            //妙！
+            
             if(i >= 2) {
                 int val = 10 * (word[i - 2] - '0') + (word[i - 1] - '0');
                 if(val >= 10 && val <= 26) {
                     f[i] += f[i - 2];
                 }
             }
-            
         }
         
         return f[len];
--- Dynamic_Programming/坐标型动态规划/Minimum_Path_Sum.java
+++ Dynamic_Programming/坐标型动态规划/Minimum_Path_Sum.java
@@ -1,11 +1,14 @@
-/* Minimum Path Sum 
-
+/* Minimum Path Sum
 Given a m x n grid filled with non-negative numbers, 
 find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
 Notice
 You can only move either down or right at any point in time.
 */
 
+/* Time: O(mn)
+   Space: O(n)
+*/
 public class Solution {
     /**
      * @param grid: a list of lists of integers.
@@ -13,26 +16,44 @@ public class Solution {
      */
     public int minPathSum(int[][] grid) {
         // write your code here
+        
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
         int m = grid.length;
         int n = grid[0].length;
-        int[][] sum = new int[m][n];
         
-        sum[0][0] = grid[0][0];
         
-        for(int i = 1; i < m; i++) {
-            sum[i][0] = sum[i - 1][0] + grid[i][0];
-        }
+        int[][] f = new int[2][n];
+        int now = 0, old = 0;
         
-        for(int j = 1; j < n; j++) {
-            sum[0][j] = sum[0][j - 1] + grid[0][j];
-        }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                sum[i][j] = Math.min(sum[i - 1][j], sum[i][j - 1]) + grid[i][j];
+        for(int i = 0; i < m; i++) {
+            old = now;
+            now = 1 - now;
+            for(int j = 0; j < n; j++) {
+                int minPre = -1;
+                if(i != 0) {
+                    if(minPre == -1) {
+                        minPre = f[old][j];
+                    }
+                }
+                
+                if(j != 0) {
+                    if(minPre == -1) {
+                        minPre = f[now][j - 1];
+                    }else {
+                        minPre = Math.min(minPre, f[now][j - 1]);
+                    }
+                }
+                
+                if(minPre == -1) {
+                    minPre = 0;
+                }
+                
+                f[now][j] = minPre + grid[i][j]; //记住， 永远是求f[newLine][j], f[oldLine][j]只是铺垫
             }
         }
         
-        return sum[m - 1][n - 1];
+        return f[now][n - 1];
     }
 }
