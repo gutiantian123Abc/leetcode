b21042fc080093dbc6396f8e5a09519bfbb65849
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-09-13 05:09:26 -0400

    is subsequence

---
+++ LinkedListAndArray/Array/TwoPointers/is_subSequence.java
@@ -0,0 +1,38 @@
+/* Is Subsequence
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. 
+t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string 
+by deleting some (can be none) of the characters without disturbing the relative 
+positions of the remaining characters. 
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+*/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s == null || t == null || s.length() > t.length()) {
+            return false;
+        }
+    
+        int p1 = 0, p2 = 0;
+        while(p1 < s.length() && p2 < t.length()) {
+            if(s.charAt(p1) == t.charAt(p2)) {
+                p1++;
+            }
+            p2++;
+        }
+        return p1 == s.length();
+    }
+}
