1ecac8ca2c53c624a167cd192f058c1b0b941737
gutiantian123Abc <william.guxiangtian@gmail.com>
2017-09-14 05:56:26 -0400

    Game if Life

---
+++ LinkedListAndArray/Array/Game_of_Life.java
@@ -0,0 +1,86 @@
+/* Game of Life
+According to the Wikipedia's article: "The Game of Life, also known simply as Life, 
+is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0). 
+Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the 
+following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population..
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+Write a function to compute the next state (after one update) of the board given its current state.
+*/
+
+class Solution {
+    private class Pair {
+        int prev = 0;
+        int next = 0;
+        public Pair(int prev, int next) {
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+    
+    int[] dx = {1, 1, 1, -1, -1, -1, 0, 0};
+    int[] dy = {1, -1, 0, 0, 1,  -1, 1, -1};
+    
+    private boolean inBound(int i, int j, int m, int n) {
+        return i >= 0 && i < m && j >= 0 && j < n;
+    }
+    
+    private int neighbor_num(int i, int j, Pair[][] A, int m, int n) {
+        int count = 0;
+        for(int dir = 0; dir < 8; dir++) {
+            int nx = i + dx[dir];
+            int ny = j + dy[dir];
+            if(inBound(nx, ny, m, n)) {
+                count += A[nx][ny].prev;
+            }
+        }
+        return count;
+    }
+    
+    public void gameOfLife(int[][] board) {
+        if(board == null || board.length < 1 || board[0].length < 1) {
+            return;
+        }
+        
+        int m = board.length, n = board[0].length;
+        
+        Pair[][] A = new Pair[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                A[i][j] = new Pair(board[i][j], 0); //一定要注意initialize !!!
+            }
+        }
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int neighbor_count = neighbor_num(i, j, A, m, n);
+                if(A[i][j].prev == 1) { //live 
+                    if(neighbor_count < 2) {
+                        A[i][j].next = 0;
+                    }else if(neighbor_count == 2 || neighbor_count == 3) {
+                        A[i][j].next = 1;
+                    }else {
+                        A[i][j].next = 0;
+                    }  
+                }else {
+                    if(neighbor_count == 3) {
+                        A[i][j].next = 1;
+                    }else {
+                        A[i][j].next = 0;
+                    }
+                }
+            }
+        }       
+         
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                board[i][j] = A[i][j].next;
+            }
+        }    
+    }
+}
