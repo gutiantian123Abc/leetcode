55b4abdaa3c6c9bb91d86a1d7525b092976f19cc
gutiantian <william.guxiangtian@gmail.com>
2020-09-27 16:30:39 -0400

    Update BlackJackCard.java
    hhcdsuchucscnsuicnws

--- Amazon/CardGame/src/BlackJackCard.java
+++ Amazon/CardGame/src/BlackJackCard.java
@@ -32,6 +32,6 @@ public class BlackJackCard extends Card{
         return value();
     }
 
-    //hahaahahahahahahahahahaahahahahahahah
+    //hahaahahahahahahahahahaahahahahahahahxxxxxxxxxxxxx
     }
 }
