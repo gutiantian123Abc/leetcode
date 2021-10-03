bf3dc5e5ac55f852378fbe748e8db194fbeb68e7
Xiangtian Gu <william.guxiangtian@gmail.com>
2021-10-03 13:46:55 -0400

    More SQL

---
+++ SQL/CustomersWhoNeverOrder.sql
@@ -0,0 +1,72 @@
+/* 183. Customers Who Never Order
+https://leetcode.com/problems/customers-who-never-order/
+
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| Id          | int     |
+| Name        | varchar |
++-------------+---------+
+Id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| Id          | int  |
+| CustomerId  | int  |
++-------------+------+
+Id is the primary key column for this table.
+CustomerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+-- Sokution: Using sub-query and NOT IN clause
+# Write your MySQL query statement below
+select Customers.name as Customers
+from Customers
+where Customers.id not in
+(
+    select CustomerId from Orders
+);
