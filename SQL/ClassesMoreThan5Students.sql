920020f22293a5048d35a845002cafbaea55b7f1
Xiangtian Gu <william.guxiangtian@gmail.com>
2021-10-03 14:51:39 -0400

    Create ClassesMoreThan5Students.sql
    596. Classes More Than 5 Students

---
+++ SQL/ClassesMoreThan5Students.sql
@@ -0,0 +1,64 @@
+/* 596. Classes More Than 5 Students
+https://leetcode.com/problems/classes-more-than-5-students/
+
+There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+ 
+
+Note:
+The students should not be counted duplicate in each course.
+
+*/
+
+-- Solution 1:
+# Write your MySQL query statement below
+select 
+  T.class 
+from 
+  (
+    select 
+      class, 
+      count(distinct student) as num 
+    from 
+      courses 
+    group by 
+      class
+  ) as T 
+where 
+  T.num >= 5;
+
+
+-- Solution 2:
+# Write your MySQL query statement below
+select 
+  class 
+from 
+  courses 
+group by 
+  class 
+having 
+  count(distinct student) >= 5
