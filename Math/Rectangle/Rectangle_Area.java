24a35314a4a16f99acdb1d530c09b9710c35a180
Xiangtian Gu <william.guxiangtian@gmail.com>
2021-10-08 14:53:05 -0400

    Last Min Warm Up before interview

--- Math/Rectangle/Rectangle_Area.java
+++ Math/Rectangle/Rectangle_Area.java
@@ -1,4 +1,5 @@
 /* Rectangle Area
+https://leetcode.com/problems/rectangle-area/
 
 Find the total area covered by two rectilinear rectangles in a 2D plane.
 
@@ -9,20 +10,20 @@ Assume that the total area is never beyond the maximum possible value of int.
 */
 //https://leetcode.com/problems/rectangle-area/description/
 class Solution {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int h1 = D - B, w1 = C - A;
-        int h2 = H - F, w2 = G - E;
-        int A1 = h1 * w1, A2 = h2 * w2;
-        int overlapped = 0;
-        int left = Math.max(A, E);
-        int right = Math.min(C, G);
-        int top = Math.min(D, H);
-        int bottom = Math.max(B, F);
-
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int areaRec1 = (ax2 - ax1) * (ay2 - ay1);
+        int areaRec2 = (bx2 - bx1) * (by2 - by1);
+        
+        int left = Math.max(ax1, bx1);
+        int right = Math.min(ax2, bx2);
+        int top = Math.min(ay2, by2);
+        int bottom = Math.max(ay1, by1);
+        
+        int overlapArea = 0;
         if(left < right && bottom < top) {
-            overlapped = (right - left) * (top - bottom);
+            overlapArea = (right - left) * (top - bottom);
         }
         
-        return A1 + A2 - overlapped;
+        return areaRec1 + areaRec2 - overlapArea;
     }
 }
