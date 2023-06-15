910dbb5ed8fee623e2438512e66c3fd9ed90d89e
gutiantian <william.guxiangtian@gmail.com>
2023-06-14 21:39:24 -0400

    603. Consecutive Available Seats

---
+++ SQL/603_Consecutive_Available_Seats.sql
@@ -0,0 +1,54 @@
+/*
+603. Consecutive Available Seats
+https://leetcode.com/problems/consecutive-available-seats/description/
+
+Table: Cinema
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment primary key column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+ 
+
+Write an SQL query to report all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+The test cases are generated so that more than two seats are consecutively available.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Cinema table:
++---------+------+
+| seat_id | free |
++---------+------+
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
++---------+------+
+Output: 
++---------+
+| seat_id |
++---------+
+| 3       |
+| 4       |
+| 5       |
++---------+
+*/
+
+# Write your MySQL query statement below
+select distinct A.seat_id
+from Cinema as A inner join Cinema as B
+on abs(A.seat_id - B.seat_id) = 1 and A.free = 1 and B.free = 1
+order by A.seat_id asc
