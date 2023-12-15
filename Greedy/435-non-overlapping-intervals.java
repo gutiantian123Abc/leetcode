3ea214014a8f87d560c61d42cffea3a29131c6a1
gutiantian <william.guxiangtian@gmail.com>
2023-12-15 11:12:56 -0500

    Create 435-non-overlapping-intervals.java

---
+++ Greedy/435-non-overlapping-intervals.java
@@ -0,0 +1,93 @@
+/* 435. Non-overlapping Intervals
+https://leetcode.com/problems/non-overlapping-intervals/description/
+
+Given an array of intervals intervals where intervals[i] = [starti, endi], 
+return the minimum number of intervals you need to remove to make the 
+rest of the intervals non-overlapping.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 105
+intervals[i].length == 2
+-5 * 104 <= starti < endi <= 5 * 104
+*/
+
+
+class Solution {
+    class Interval {
+        int start;
+        int end;
+        public Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int removedNum = 0;
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> {
+            if(a.end == b.end) {
+                return a.start - b.start; // doesn't matter a.start - b.start or b.start - a.start
+            } else {
+                return a.end - b.end;
+            }
+        });
+
+
+        for(int i = 0; i < intervals.length; i++) {
+            pq.offer(new Interval(intervals[i][0], intervals[i][1]));
+        }
+
+        Interval firstInterval = pq.poll();
+        int prevEnd = firstInterval.end;
+        
+        while(!pq.isEmpty()) {
+            Interval interval = pq.poll();
+            int curStart = interval.start;
+            int curEnd = interval.end;
+
+            if(curStart < prevEnd) {
+                removedNum++;
+            } else {
+                prevEnd = curEnd;
+            }
+        }
+
+        return removedNum;
+    }
+}
+
+/* Tutorial:
+The primary reason to sort by end times first in this problem 
+is to ensure that you are always considering the interval that 
+finishes earliest as your first choice. This approach minimizes 
+the chance of overlap with subsequent intervals, as an interval 
+that ends earlier frees up more space for others.
+
+Sorting by start time when end times are the same, however, 
+is a secondary criterion and is less critical for the correctness 
+of the solution. The main idea behind this secondary sorting is to 
+maintain a consistent and predictable ordering of intervals that 
+share the same end time. This can be helpful in certain edge cases, 
+but for the majority of scenarios in this particular problem, 
+it won't make a difference in the outcome.
+*/
