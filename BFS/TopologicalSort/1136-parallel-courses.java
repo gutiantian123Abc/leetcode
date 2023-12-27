681992e44dc533a829a7ee1a4719f91c36e30ba2
gutiantian <william.guxiangtian@gmail.com>
2023-12-27 12:06:35 -0500

    TopLogical Sort

---
+++ BFS/TopologicalSort/1136-parallel-courses.java
@@ -0,0 +1,104 @@
+/* 1136. Parallel Courses
+
+https://leetcode.com/problems/parallel-courses/
+
+You are given an integer n, which indicates that there are n courses 
+labeled from 1 to n. You are also given an array relations where 
+relations[i] = [prevCoursei, nextCoursei], representing a prerequisite 
+relationship between course prevCoursei and course nextCoursei: course 
+prevCoursei has to be taken before course nextCoursei.
+
+In one semester, you can take any number of courses as long as you have 
+taken all the prerequisites in the previous semester for the courses you are taking.
+
+Return the minimum number of semesters needed to take all courses. 
+If there is no way to take all the courses, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, relations = [[1,3],[2,3]]
+Output: 2
+Explanation: The figure above represents the given graph.
+In the first semester, you can take courses 1 and 2.
+In the second semester, you can take course 3.
+Example 2:
+
+
+Input: n = 3, relations = [[1,2],[2,3],[3,1]]
+Output: -1
+Explanation: No course can be studied because they are prerequisites of each other.
+ 
+
+Constraints:
+
+1 <= n <= 5000
+1 <= relations.length <= 5000
+relations[i].length == 2
+1 <= prevCoursei, nextCoursei <= n
+prevCoursei != nextCoursei
+All the pairs [prevCoursei, nextCoursei] are unique.
+*/
+
+class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        //1. map<Integer, Integer> parentsMap
+        Map<Integer, List<Integer>> parentsMap = new HashMap<>();
+        //2. map<Integer, Integer> childrenMap
+        Map<Integer, List<Integer>> childrenMap = new HashMap<>();
+
+        for(int i = 0; i < relations.length; i++) {
+            int parent = relations[i][0];
+            int child = relations[i][1];
+
+            if(!parentsMap.containsKey(child)) {
+                parentsMap.put(child, new ArrayList<>());
+            }
+            parentsMap.get(child).add(parent);
+
+            if(!childrenMap.containsKey(parent)) {
+                childrenMap.put(parent, new ArrayList<>());
+            }
+            childrenMap.get(parent).add(child);
+        }
+
+        // BFS
+        int S = 0;
+        int couurseNum = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i = 1; i <= n; i++) {
+            if(!parentsMap.containsKey(i)) {
+                queue.add(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                int parent = queue.poll();
+                couurseNum++;
+                List<Integer> children = childrenMap.get(parent);
+                if(children != null) {
+                    for(int child : childrenMap.get(parent)) {
+                    	// Important!!!! Indegree
+                        int parentIndex = parentsMap.get(child).indexOf(parent);
+                        parentsMap.get(child).remove(parentIndex);
+                        if(parentsMap.get(child).size() == 0) {
+                            queue.add(child);
+                        }
+                    }
+                }
+            }
+            S++;
+        }
+
+        if(couurseNum != n) {
+            return -1;
+        }
+
+        return S;
+    }
+}
