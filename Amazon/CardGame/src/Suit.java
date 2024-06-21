77a2fcd4978923d4e5db87975ca90c85fcbc6eb8
Xiangtian Gu <william.guxiangtian@gmail.com>
2024-06-21 17:25:21 -0400

    Removed

--- Amazon/CardGame/src/Suit.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xiangtiangu on 4/26/17.
- */
-public enum Suit {
-    Club,
-    Diamond,
-    Heart,
-    Spade
-}
--- Amazon/Elevator/src/User.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xiangtiangu on 4/25/17.
- */
-public class User {
-    public void pressButton(int toFloor) {
-        Request request = new Request(toFloor);
-        RequestCommandCenter.getInstance().addRequest(request);
-    }
-}
